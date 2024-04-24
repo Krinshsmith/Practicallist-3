da099c0900b6caa17405d05b75f618f2a1ec6740
Krinshsmith Kava <141899936+Krinshsmith@users.noreply.github.com>
2024-04-24 21:07:37 +0530

    Create Program-10.java
    Each subclass should have its own specific attributes and 
    methods. For example: 
     Circle should have a radius attribute and a 
    method to calculate its area. 
     Rectangle should have attributes for its width 
    and height, and methods to calculate its area and 
    perimeter. 
     Triangle should have attributes for its base and 
    height, and methods to calculate its area and 
    perimeter.

---
+++ Program-10.java
@@ -0,0 +1,75 @@
+class S
+{
+    public void display()
+    {
+        System.out.println("Shape Class");
+    }
+}
+
+class Circle extends S 
+{
+    double r;
+
+    double area() 
+    {
+        return Math.PI * r * r;
+    }
+}
+
+class Reactangle extends S 
+{
+    double w;
+    double h;
+
+    double area()
+    {
+        return w * h;
+    }
+
+    double perimeter() 
+    {
+        return 2 * (w + h);
+    }
+}
+
+class Triangle extends S 
+{
+    double b;
+    double h;
+
+    double area()
+    {
+        return 0.5 * b * h;
+    }
+
+    double perimeter()
+    {
+        return 3 * b;
+    }
+}
+
+public class p10
+{
+    public static void main(String[] args) 
+  {
+         System.out.println("My Name Is Krinshsmith Kava");
+         System.out.println("Er_no :: 220130318102");  
+
+        Circle c = new Circle();
+        c.r = 5.0;
+        c.display();
+        System.out.println("Area of circle: " + c.area());
+
+        Reactangle r = new Reactangle();
+        r.w = 4.0;
+        r.h = 6.0;
+        System.out.println("Area of rectangle: " + r.area());
+        System.out.println("Perimeter of rectangle: " + r.perimeter());
+
+        Triangle t = new Triangle();
+        t.b = 3.0;
+        t.h = 4.0;
+        System.out.println("Area of triangle: " + t.area());
+        System.out.println("Perimeter of triangle: " + t.perimeter());
+    }
+}
