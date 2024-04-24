cecbfec3b05c607376a31f6da0b46167f5f0a128
Krinshsmith Kava <141899936+Krinshsmith@users.noreply.github.com>
2024-04-24 20:32:37 +0530

    Update Program-1.java
    Write a Java Program to explain super keyword.

--- Program-1.java
+++ Program-1.java
@@ -1,1 +1,22 @@
+public class p1 {
+    public static void main(String[] args) {
 
+      System.out.println("My Name Is Krinshsmith Kava");
+      System.out.println("Er_no :: 220130318102");
+        ChildClass obj = new ChildClass();
+        obj.display();
+    }
+}
+
+class ParentClass {
+    int num = 100;
+}
+
+class ChildClass extends ParentClass {
+    int num = 50;
+
+    void display() {
+        System.out.println("Child class num: " + num);
+        System.out.println("Parent class num: " + super.num);
+    }
+}
