5bbe70c507dc808a5873bbccb70fada68fb52f28
Krinshsmith Kava <141899936+Krinshsmith@users.noreply.github.com>
2024-04-24 21:55:58 +0530

    Create Program-15.java
    Create two subclasses of Shape called “Rectangle” and 
    “Triangle” with the following properties and methods: 
    Rectangle: 
     A private double called “length” 
     A private double called “width” 
     A constructor that takes in two doubles (length 
    and width) and sets the corresponding fields. 
     An implementation of the calculateArea() 
    method that return length * width. 
    Triangle: 
     A private double called “base”
     A private double called “height”
     A constructor that takes in two doubles (base 
    and height) and sets the corresponding fields.
     An implementation of the calculateArea() 
    method that returns (base * height)/2.
    Create a main class called “ShapeTester” with the 
    following properties and methods: 
     A static method called “printShapeArea” that 
    takes in a Shape object and prints out the area of 
    the shape. 
     In the main method, create one Rectangle and 
    one Triangle object, pass them into the 
    printShapeArea method, and observe the output.

---
+++ Program-15.java
@@ -0,0 +1,63 @@
+abstract class Shape
+{
+    protected int numberOfSides;
+
+    abstract double calculateArea();
+}
+
+class Rectangle extends Shape 
+{
+    private double length;
+    private double width;
+
+    Rectangle(double length, double width) 
+    {
+        this.length = length;
+        this.width = width;
+        numberOfSides = 4;
+    }
+
+    double calculateArea() 
+    {
+        return length * width;
+    }
+}
+
+class Triangle extends Shape
+{
+    private double base;
+    private double height;
+
+    Triangle(double base, double height)
+    {
+        this.base = base;
+        this.height = height;
+        numberOfSides = 3;
+    }
+
+    double calculateArea() 
+    {
+        return (base * height) / 2;
+    }
+}
+
+public class ShapeTester 
+{
+    public static void printShapeArea(Shape shape) 
+    {
+        System.out.println("Area of the shape: " + shape.calculateArea());
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("My Name Is Krinshsmith Kava");
+        System.out.println("Er_no :: 220130318102");
+        Rectangle rectangle = new Rectangle(4, 6);
+        Triangle triangle = new Triangle(3, 4);
+
+        System.out.println("Area of Reactangle is :: ");
+        printShapeArea(rectangle);
+        System.out.println("Area of traianle is :: ");
+        printShapeArea(triangle);
+    }
+}
