a7e23aaa4656c77877b0fb32b856cd5e99882d2e
Krinshsmith Kava <141899936+Krinshsmith@users.noreply.github.com>
2024-04-24 20:37:12 +0530

    Create Program-2.java
    Create a class to find out whether the given year is 
    leaping year or not. 
    (Use inheritance for this program)

---
+++ Program-2.java
@@ -0,0 +1,44 @@
+class Year 
+{
+    int year;
+
+    Year(int year) 
+    {
+        this.year = year;
+    }
+
+    boolean isLeapYear() 
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            return true;
+        else
+            return false;
+    }
+}
+
+class LeapYearChecker extends Year 
+{
+    LeapYearChecker(int year) 
+    {
+        super(year);
+    }
+
+    void checkLeapYear() 
+    {
+        if (isLeapYear())
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+}
+
+public class p2
+{
+    public static void main(String[] args) 
+    {
+        LeapYearChecker yearObj = new LeapYearChecker(2024);
+        yearObj.checkLeapYear();
+        System.out.println("My Name Is Krinshsmith Kava");
+        System.out.println("Er_no :: 220130318102");
+    }
+}
