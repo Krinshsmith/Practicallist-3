059fcb7a60e23995f8a09e0e1e2fc03f6e170747
Krinshsmith Kava <141899936+Krinshsmith@users.noreply.github.com>
2024-04-24 21:02:32 +0530

    Create Program-6.child.java

---
+++ Program-6.child.java
@@ -0,0 +1,13 @@
+package package2;
+
+import package1.p6_Parent;
+
+public class p6_Child extends p6_Parent
+  {
+     System.out.println("My Name Is Krinshsmith Kava");
+     System.out.println("Er_no :: 220130318102");
+     public void display() 
+    {
+        System.out.println("This is child class overriding the parent method.");
+    }
+}
